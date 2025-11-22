5bf453c6dd9d2b35df4a6736aa91a6917b25ac0a
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2025-11-22 15:40:02 +0300

    Refactor CSS variables and enhance App component layout with responsive design and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import DesktopIllustration from "./assets/images/illustration-sign-up-desktop.svg";
 import TabletIllustration from "./assets/images/illustration-sign-up-tablet.svg";
 import MobileIllustration from "./assets/images/illustration-sign-up-mobile.svg";
+import RightIcon from "./assets/images/icon-list.svg";
 import "./App.css";
 
 function App() {
+  const [isValid, setIsvalid] = useState(true);
   return (
-    <>
+    <div className="main-container">
       <picture className="illustration-image">
         <source media="(min-width: 1024px)" srcSet={DesktopIllustration} />
-        <source media="(min-width: 640px)" srcSet={TabletIllustration} />
+        <source media="(min-width: 768px)" srcSet={TabletIllustration} />
         <img src={MobileIllustration} />
       </picture>
 
@@ -21,23 +23,39 @@ function App() {
 
         <ul className="feature-list">
           <li className="feature-item">
-            Product discovery and building what matters
+            <img src={RightIcon} alt="" className="right-icon" />
+            <span>Product discovery and building what matters</span>
           </li>
           <li className="feature-item">
-            Measuring to ensure updates are a success
+            <img src={RightIcon} alt="" className="right-icon" />
+            <span>Measuring to ensure updates are a success</span>
           </li>
-          <li className="feature-item">And much more!</li>
+          <li className="feature-item">
+            <img src={RightIcon} alt="" className="right-icon" />
+            <span>And much more!</span>
+          </li>
         </ul>
 
         <form className="form-control">
-          <label htmlFor="email-input">Email address</label>
-          <input type="email" name="" id="email-input" />
+          <div htmlFor="" className="labels">
+            <label htmlFor="input-field" className="email-text">
+              Email address
+            </label>
+            {!isValid && <p className="error">Valid email required!</p>}
+          </div>
+          <input
+            type="email"
+            name=""
+            id="input-field"
+            placeholder="email@company.com"
+            className={isValid ? "input-field " : "input-field error-input "}
+          />
           <button className="submit-btn">
             Subscribe to monthly newsletter
           </button>
         </form>
       </div>
-    </>
+    </div>
   );
 }
 
