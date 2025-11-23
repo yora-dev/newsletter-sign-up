f7f3fe3c70c266eb5e4d371b864498ad619a6e17
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2025-11-23 09:55:38 +0300

    Add success message component with styling and layout adjustments

--- src/SuccessMsg.jsx
+++ src/SuccessMsg.jsx
@@ -1,5 +1,22 @@
-function SuccessMsg() {
-  return <div>SuccessMsg</div>;
+import Success from "./assets/images/icon-success.svg";
+import "./App.css";
+
+function SuccessMsg({ email, onDismiss }) {
+  return (
+    <div className="success-container">
+      <img src={Success} alt="" className="success-img" />
+      <h1 className="success-header"> Thanks for subscribing!</h1>
+      <p className="success-msg">
+        A confirmation email has been sent to{" "}
+        <span className="email"> {email}. </span> Please open it and click the
+        button inside to confirm your subscription.
+      </p>
+
+      <button className="dismiss-btn" onClick={onDismiss}>
+        Dismiss messages
+      </button>
+    </div>
+  );
 }
 
 export default SuccessMsg;
